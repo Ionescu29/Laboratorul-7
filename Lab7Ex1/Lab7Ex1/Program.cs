c4fabad978fd7343e09fcc24cbe051e0968111a4
Ionescu29 <160519352+Ionescu29@users.noreply.github.com>
2024-04-08 19:30:13 +0200

    Add files via upload

---
+++ Lab7Ex1/Lab7Ex1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab7Ex1;
+
+Console.WriteLine("Hello, World!");
+//Un autoturism detine un motor precum si un numar de usi. De asemenea autovehiculul este
+//caracterizat de marca, model si an de fabricatie
+//Autoturismul va avea urmatoarele metode / proprietati
+//Va returna descrierea completa a autovehicolului sub forma unui sir de caractere
+//Marca, model, anDeFabricatie, capacitate cilindrica, combustibil, putere motor, numar de usi'
+// am facut o a doua clasa "autoturism" cu urmatoarele caracteristici
+//marca
+//model
+//an de fabricatie
+//numar de usi
+Console.WriteLine(Autoturism.GetDescription);
+class Autoturism
+{
+    private string marca;
+    private string model;
+    private int anDeFabricatie;
+    private int numarDeUsi;
+    public Autoturism(string marca, string model, int anDeFabricatie, int numarDeUsi)
+    {
+        marca = "Nissan";
+        model = "Juke";
+        anDeFabricatie = 2012;
+        numarDeUsi = 4;
+    }
+    public string GetDescription()
+    {
+        return $"{marca}, {model}, {anDeFabricatie}, {capacitateCilindrica}, {putere}, {tipulCombustibilului}, {numarDeUsi}";
+    }
+}
---
+++ Lab7Ex1/Lab7Ex1/motor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7Ex1
+{
+    //am facut o clasa separata "motor" cu urmatoarele caracteristici
+    // capacitate cilindrica
+    // putere
+    // tipul combustibilului
+    internal class Motor
+    {
+        private int capacitateCilindrica;
+        private int putere;
+        private string tipulCombustibilului;
+        public Motor(int capacitateCilindrica, int putere, string tipulCombustibilului)
+        {
+            capaciateCilindrica = 2000;
+            putere = 150;
+            tipulCombustibilului = "motorina";
+        }
+        
+    }
+}
